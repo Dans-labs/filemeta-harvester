0e057f19739647a8598b8174142660ec475e9f79
Reggie Cushing <r.cushing@esciencecenter.nl>
2026-01-13 11:56:05 +0100

    Remove worklow orchetrator: prefect

--- src/filemeta_harvester/flows/harvester_flows.py
+++ src/filemeta_harvester/flows/harvester_flows.py
@@ -1,6 +1,6 @@
 import datetime
-from prefect import flow
-# from filemeta_harvester.config import load_endpoints_config
+from filemeta_harvester.config import load_endpoints_config
+from multiprocessing import Process
 from filemeta_harvester.tasks.harvester_tasks import (
             initialize_db, 
             initialize_file_db, 
@@ -9,13 +9,10 @@ from filemeta_harvester.tasks.harvester_tasks import (
             process_pending_pids
 )
 
-# endpoints_config = load_endpoints_config()
 
-# def generate_timestamp():
-#     return datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+def generate_timestamp():
+    return datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-# @flow(flow_run_name="-{id}-", log_prints=True)
-@flow(log_prints=True)
 def filemeta_harvest_flow(endpoint: dict):
     print(f"Starting harvest flow for endpoint: {endpoint['name']}")
     check_endpoint(endpoint["oai_url"], endpoint["name"], endpoint.get("metadata_prefix", "oai_dc"))
@@ -26,6 +23,12 @@ def filemeta_harvest_flow(endpoint: dict):
     process_pending_pids(endpoint['id'])
 
 
-# if __name__ == "__main__":
-#     for endpoint in endpoints_config:
-#         filemeta_harvest_flow(endpoint, endpoint['id'])
+if __name__ == "__main__":
+    endpoints_config = load_endpoints_config()
+    deployed_flows = []
+    for endpoint in endpoints_config:
+        p = Process(target=filemeta_harvest_flow, args=(endpoint,))
+        p.start()
+        deployed_flows.append(p)
+    for p in deployed_flows:
+        p.join()
